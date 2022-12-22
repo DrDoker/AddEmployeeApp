5ba04b92cdf9a78f0749d5c5a4c0c85741a45766
Serhii Tkachenko <doker.tk@gmail.com>
2022-12-22 13:45:38 +0100

    Initial Commit

---
+++ AddEmployeeApp/AddEmployeeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AddEmployeeAppApp.swift
+//  AddEmployeeApp
+//
+//  Created by Serhii  on 22/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct AddEmployeeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
